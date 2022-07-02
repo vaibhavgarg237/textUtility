a97ffcb5113ea96b2c89e66b91a9edc28c81729d
Vaibhav Garg <67408018+vaibhavgarg237@users.noreply.github.com>
2022-07-02 21:35:35 +0530

    nits

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -4,7 +4,7 @@ import PropTypes from "prop-types";
 function Navbar(props) {
   return (
     <div>
-      <nav className="navbar navbar-expand-lg bg-light">
+      <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
         <div className="container-fluid">
           <a className="navbar-brand" href="/">
             {props.title}
--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -5,28 +5,45 @@ function TextForm(props) {
 
   return (
     <>
-      <h1>{props.heading}</h1>
-      <div className="mb-3">
-        <textarea
-          className="form-control"
-          placeholder="Leave a comment here"
-          id="myBox"
-          rows="3"
-          value={text}
-          onChange={(event) => {
-            setText(event.target.value);
+      <div className="container">
+        <h1>{props.heading}</h1>
+        <div className="mb-3">
+          <textarea
+            className="form-control"
+            placeholder="Leave a comment here"
+            id="myBox"
+            rows="3"
+            value={text}
+            onChange={(event) => {
+              setText(event.target.value);
+            }}
+          ></textarea>
+        </div>
+        <button
+          className="btn btn-primary mx-1"
+          onClick={() => {
+            setText(text.toLocaleUpperCase());
           }}
-        ></textarea>
+        >
+          Convert to upper case
+        </button>
+        <button
+          className="btn btn-primary mx-1"
+          onClick={() => {
+            setText(text.toLocaleLowerCase());
+          }}
+        >
+          Convert to lower case
+        </button>
       </div>
-      <button
-        className="btn btn-primary"
-        onClick={() => {
-          //change text to upper case
-          setText(text.toLocaleUpperCase());
-        }}
-      >
-        Convert to upper case
-      </button>
+      <div className="container my-3">
+        <h2>Your Text Summary</h2>
+        <p>
+          {text.split(" ").length} Words, {text.length} letters
+        </p>
+        <h2>Preview</h2>
+        <p>{text}</p>
+      </div>
     </>
   );
 }
