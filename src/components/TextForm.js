72ff662ecc4061f2e71fea9bca4a50fbf9166f80
Vaibhav Garg <67408018+vaibhavgarg237@users.noreply.github.com>
2022-07-02 15:37:23 +0530

    textform basic

---
+++ src/components/TextForm.js
@@ -0,0 +1,21 @@
+import React from "react";
+
+function TextForm() {
+  return (
+    <div>
+      <div className="mb-3">
+        <label for="exampleFormControlInput1" className="form-label">
+          Email address
+        </label>
+        <input
+          type="email"
+          className="form-control"
+          id="exampleFormControlInput1"
+          placeholder="name@example.com"
+        />
+      </div>
+    </div>
+  );
+}
+
+export default TextForm;
